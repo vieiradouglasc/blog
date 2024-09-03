ed3d25d08ecf686bed2f48bc23561fe141b3bb08
vieiradouglasc <vieira.douglasc@gmail.com>
2024-09-02 22:19:16 -0300

    criação de tabela categorias com acesso ao bando de dados

--- resources/views/categoria/index.blade.php
+++ resources/views/categoria/index.blade.php
@@ -6,6 +6,23 @@
     <h1>Categorias</h1>
 @stop
 
+@php
+    $heads = [
+        // representa o thead da table
+        'ID',
+        'Nome',
+        'Ações',
+    ];
+@endphp
+
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <x-adminlte-datatable id="table1" :heads="$heads">
+        @foreach ($categorias as $categoria)
+            <tr>
+                <td>{{$categoria->id}}</td>
+                <td>{{$categoria->nome_categoria}}</td>
+                <td>-</td>
+            </tr>
+        @endforeach
+    </x-adminlte-datatable>
 @stop
