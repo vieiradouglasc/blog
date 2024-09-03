66f1c72fcebccea8c13c21afa00ee86375774039
vieiradouglasc <vieira.douglasc@gmail.com>
2024-09-02 22:00:04 -0300

    Criação tabela de categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
     public function index() {
-        return view('categoria.index');
+        $categorias = Categoria::all();
+        return view('categoria.index', compact('categorias'));
     }
 }
+
+
