6865caef8ca3f3b4b623dcb7ff44ab602ff7a7bf
vieiradouglasc <vieira.douglasc@gmail.com>
2024-09-03 15:12:55 -0300

    formatação código

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -7,25 +7,30 @@ use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $categorias = Categoria::all();
         return view('categoria.index', compact('categorias'));
     }
 
-    public function formularioCriarCategoria() {
+    public function formularioCriarCategoria()
+    {
         return view('categoria.criar-categoria');
     }
 
-    public function criarCategoria(Request $request) {
+    public function criarCategoria(Request $request)
+    {
         Categoria::create($request->all());
         return redirect()->route('categoria.index');
     }
 
-    public function formularioAtualizarCategoria (Categoria $categorias) {
+    public function formularioAtualizarCategoria(Categoria $categorias)
+    {
         return view('categoria.atualizar-categoria', compact('categorias'));
     }
 
-    public function atualizarCategoria (Request $request, Categoria $categorias) {
+    public function atualizarCategoria(Request $request, Categoria $categorias)
+    {
         $categorias->update($request->all());
         return redirect()->route('categoria.index');
     }
