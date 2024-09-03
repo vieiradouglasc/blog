cfa2f9b89e482bcfcecc9b7cf859d2f03d7209bb
vieiradouglasc <vieira.douglasc@gmail.com>
2024-09-02 21:33:41 -0300

    criação de tabela de categorias

---
+++ resources/views/categoria/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Categorias')
+
+@section('content_header')
+    <h1>Categorias</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('categorias',[CategoriaController::class, 'index'])->name('categoria.index');
